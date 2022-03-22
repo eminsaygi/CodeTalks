abee844cfc25ea72c2fce2f39cd398ae27b6c8ff
Emin Saygı <emin_0001@hotmail.com>
2022-03-22 18:28:37 +0300

    March 22, 2022 6:28 PM

--- src/components/modal/ContentInputModal/ContentInputModal.js
+++ src/components/modal/ContentInputModal/ContentInputModal.js
@@ -1,21 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, TextInput} from 'react-native';
 import Modal from 'react-native-modal';
 
+import Button from '';
 import styles from './ContentInputModal.style';
-import Button from '../../Button/LoginButton';
 
-const ContentInputModal = ({visible, onClose, onSend}) => {
-  const [text, setText] = React.useState(null);
+const ContentInputModal = ({visible, onClose, onSend, placeholder}) => {
+  const [text, setText] = useState('');
 
-  function handleSend() {
+  const handleSend = () => {
     if (!text) {
       return;
     }
-
     onSend(text);
     setText(null);
-  }
+  };
+
   return (
     <Modal
       style={styles.modal}
@@ -23,18 +23,21 @@ const ContentInputModal = ({visible, onClose, onSend}) => {
       swipeDirection="down"
       onSwipeComplete={onClose}
       onBackdropPress={onClose}
-      onBackButtonPres={onClose}>
+      onBackButtonPress={onClose}>
       <View style={styles.container}>
         <View style={styles.input_container}>
-          <TextInput
-            placeholder="Derdin umrumda değil ama anlat.."
+        <TextInput
+            style={styles.input_text}
+            placeholder={placeholder}
             onChangeText={setText}
+            placeholderTextColor="#bbdefb"
             multiline
           />
         </View>
-        <Button text="Gönder" onPress={handleSend}></Button>
+        <Button text={'Send'} onPress={handleSend} />
       </View>
     </Modal>
   );
 };
+
 export default ContentInputModal;
